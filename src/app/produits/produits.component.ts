6252c77001cdb80c07e6bd105bfa801b46d7c6b4
sarrajerbi <sarrajerbi99@gmail.com>
2023-10-03 23:45:14 +0100

    first commit

---
+++ src/app/produits/produits.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produits',
+  templateUrl: './produits.component.html'
+})
+export class ProduitsComponent  {
+    
+  produits =["PC Asus", "Imprimante Epson", "Tablette Samsung"];
+  
+ 
+}
